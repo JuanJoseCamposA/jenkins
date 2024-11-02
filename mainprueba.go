0819096d908bf45e44a7316049242e04b19b2535
JuanJoseCamposA <juanjosecampos627@gmail.com>
2024-11-01 20:10:59 -0600

    Update mainprueba.go

--- mainprueba.go
+++ mainprueba.go
@@ -335,11 +335,7 @@ func main() {
 	http.HandleFunc("/estudiantes/actividad", getEstudiantesPorActividadID(db))
 	http.HandleFunc("/estudiantes/update", updateEstudiante(db))
 
-<<<<<<< HEAD
-	log.Println("Servidor escuchando en http://localhost:8080...")
-=======
 	log.Println("Servidor escuchando en http://localhost:8081...")
->>>>>>> origin/main
 	if err := http.ListenAndServe("0.0.0.0:8081", nil); err != nil {
 		log.Fatal(err)
 	}
